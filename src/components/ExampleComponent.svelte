2f34174d4d10332a5858727ccb7f7560d5da2535
alasim <19303035@iubat.edu>
2021-02-13 13:35:59 +0600

    first commit 3

--- src/components/ExampleComponent.svelte
+++ src/components/ExampleComponent.svelte
@@ -6,7 +6,7 @@
 	<div class="w-1/3">
 		<div>
 			<h1 class="text-lg text-bold">🌐 Sapper with TypeScript and Tailwindcss project base</h1>
-            <p>This is an example route and component to make sure everything's working.Nice.</p>
+            <p>This is an example route and component to make sure everything's working.Nice! </p>
 		</div>
 	</div>
 </div>
